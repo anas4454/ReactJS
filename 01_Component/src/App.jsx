9a71219cef94fdc94e50474dc34f0bd080bef8e2
Muhammad Anas <manasali493@gmail.com>
2026-07-30 16:44:25 +0500

    Initial commit

---
+++ 01_Component/src/App.jsx
@@ -0,0 +1,16 @@
+import Basic from "./Basic";
+
+
+function App() {
+    const username = "chai aur code "
+ return(
+    <>
+ <Basic  />
+ <h2>{username}</h2> 
+    </>
+
+);
+}
+
+
+export default App
---
+++ 01_Component/src/Basic.jsx
@@ -0,0 +1,7 @@
+function Basic(){
+    return(
+        <h2> This is second file </h2>
+    )
+}
+
+export default Basic
