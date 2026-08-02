6f7f93ff50ab0e6d70d1fdcd7afad3fcf5724a52
Muhammad Anas <manasali493@gmail.com>
2026-08-02 14:00:38 +0500

    Tailwind configration in ReactJS

--- 02_hooks/src/App.jsx
+++ 02_hooks/src/App.jsx
@@ -1,36 +1,49 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+
+// function App() {
+
+//   // let counter = 10
+
+//   let [counter, setCounter] = useState(10)
+
+
+//   const addScore = () => {
+
+//     if (counter < 20) {
+//       setCounter(counter + 1)
+//     }
+//   }
+
+
+//   const removeScore = () => {
+//     if (counter > 0) {
+//       setCounter(counter - 1)
+//     }
+//   }
+
+//   return (
+//     <>
+//       <h2>This is my ReactJS Hooks code</h2>
+//       <h4>Now your score is {counter}</h4>
+
+//       <button onClick={addScore}>Add Score {counter}</button>
+//       <br />
+//       <hr />
+//       <button onClick={removeScore}>Remove Score {counter}</button>
+//     </>
+//   )
+// }
+
+// export default App
+
 
 function App() {
-
-  // let counter = 10
-
-  let [counter, setCounter] = useState(10)
-
-
-  const addScore = () => {
-
-    if (counter < 20) {
-      setCounter(counter + 1)
-    }
-  }
-
-
-  const removeScore = () => {
-    if (counter > 0) {
-      setCounter(counter - 1)
-    }
-  }
-
   return (
-    <>
-      <h2>This is my ReactJS Hooks code</h2>
-      <h4>Now your score is {counter}</h4>
-
-      <button onClick={addScore}>Add Score {counter}</button>
-      <br />
-      <hr />
-      <button onClick={removeScore}>Remove Score {counter}</button>
-    </>
+     <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-5xl font-bold text-cyan-400">
+        Tailwind CSS is Working!
+      </h1>
+    </div>
   )
 }
 
