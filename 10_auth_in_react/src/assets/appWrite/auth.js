12ff3fadac82075dfea3783af45dd39d7390e73b
Muhammad Anas <manasali493@gmail.com>
2026-08-20 13:05:40 +0500

    Auth in react Js

---
+++ 10_auth_in_react/src/assets/appWrite/auth.js
@@ -0,0 +1,71 @@
+import conf from "../../conf/conf";
+import { Client, Account ,ID } from "appwrite";
+
+
+export class AuthService {
+    Client = new Client();
+    account;
+
+    constructor(){
+        this.Client
+        .setEndpoint(conf.appWriteUrl)
+        .setProject(conf.appWriteProjectId)
+        this.account = new Account(this.Client)
+
+    }
+
+    async createAccount({email , password , name}){
+        try{
+          const userAccount = await this.account.create(ID.unique() ,email, password , name);
+
+          if(userAccount){
+           //call another method
+            return this.login({email , password});
+
+
+          }
+          else{
+            return userAccount;
+          }
+        }
+        catch(error){
+            throw error;
+        }
+    }
+
+    async login({email , password}){
+        try{
+            const userAccount = await this.account.createEmailSession(email , password);
+        
+            return userAccount;
+
+        }
+
+            catch(error){
+                throw error;
+            }      
+    }
+
+    async getCurrentUser(){
+        try{
+            return await this.account.get();
+        }
+        catch(error){
+            console.log("Appwrite service :: getCurrentUser :: error" , error);
+        }
+        return null;
+    }
+
+    async logout(){
+        try{
+            await this.account.deleteSession();
+        }
+        catch(error){
+            console.log("Appwrite service :: logout :: error" , error);
+        }
+    }
+}
+
+const authservice = new AuthService();
+
+export default AuthService
